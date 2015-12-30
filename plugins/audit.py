657e87ee91a5e2582f1639dd89a22211592ebab4
Hervé BRY <herve.bry@geneanet.org>
2015-12-30 15:41:55 +0100

    Descending order for audit log

--- plugins/audit.py
+++ plugins/audit.py
@@ -213,7 +213,7 @@ class AuditPlugin(object):
                 ] }
             },
             "sort": [
-                { '@timestamp': "asc" }
+                { '@timestamp': "desc" }
             ]
         })
 
