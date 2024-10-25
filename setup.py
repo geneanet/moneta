2c699b8c20088398b29e600f5c48ddf966037eef
Hervé BRY <herve.bry@geneanet.org>
2024-10-25 12:04:28 +0200

    Add plugins to package

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+from glob import glob
 
 here = path.abspath(path.dirname(__file__))
 
@@ -79,7 +80,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
+    data_files=[('usr/share/moneta/plugins',glob(here + '/plugins/*.py'))],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
